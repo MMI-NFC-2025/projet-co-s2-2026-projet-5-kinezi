13683d4dd4962b06b9a9826a401740e57b8ee0e1
hillairet-enzo <enzo.hillairet@edu.univ-fcomte.fr>
2026-06-04 12:23:42 +0100

    pb.js

--- src/utils/xp.js
+++ src/utils/xp.js
@@ -1,20 +1,15 @@
-import PocketBase from 'pocketbase';
+iimport { pb } from './pb.js';
 
 export async function addXp(xpReward) {
-  const pb = new PocketBase('http://pbopensigne.lilian-maitre.fr');
-  
   if (!pb.authStore.isValid || !pb.authStore.model) return;
 
   const user = pb.authStore.model;
 
   try {
-    // 1. Récupération des données fraîches
     const freshUser = await pb.collection('users').getOne(user.id);
     
-    // 2. Ajout de l'XP
     const newXp = (freshUser.xp || 0) + xpReward;
 
-    // 3. Calcul du STREAK (La flamme)
     const now = new Date();
     const todayStr = now.toISOString().split('T')[0];
     
@@ -26,30 +21,25 @@ export async function addXp(xpReward) {
 
     let newStreak = freshUser.streak || 0;
 
-    // S'il n'a pas encore joué aujourd'hui
     if (lastActivity !== todayStr) {
         if (lastActivity === yesterdayStr) {
-            // S'il a joué hier, la flamme augmente
             newStreak += 1;
         } else {
-            // S'il a raté hier, la flamme retombe à 1
             newStreak = 1;
         }
     }
 
-    // 4. Enregistrement en base de données
     const updatedUser = await pb.collection('users').update(user.id, { 
       xp: newXp,
       streak: newStreak,
       last_activity: now.toISOString()
     });
 
-    // 5. Mise à jour de la mémoire du navigateur
     const authData = JSON.parse(localStorage.getItem('pocketbase_auth') || '{}');
     authData.model = updatedUser;
     localStorage.setItem('pocketbase_auth', JSON.stringify(authData));
 
   } catch (error) {
-    console.error("Erreur lors de la sauvegarde de l'XP/Streak :", error);
+    console.error(error);
   }
 }
