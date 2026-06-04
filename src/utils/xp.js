760e326510fb6a01d53ffdcac2bbb7f76cb5c204
hillairet-enzo <enzo.hillairet@edu.univ-fcomte.fr>
2026-06-04 09:49:39 +0100

    fin

--- src/utils/xp.js
+++ src/utils/xp.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 export async function addXp(xpReward) {
-  const pb = new PocketBase('http://127.0.0.1:8090');
+  const pb = new PocketBase('http://pbopensigne.lilian-maitre.fr');
   
   if (!pb.authStore.isValid || !pb.authStore.model) return;
 
